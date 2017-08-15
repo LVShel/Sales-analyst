3a4b01c3cd225e97697067cff29de5695f661d89
LVShel <leonidshelest86@icloud.com>
2017-08-15 22:59:08 +0300

    javadoc for InitStrategy interface

--- src/main/java/com/company/utils/InitStrategy/InitStrategy.java
+++ src/main/java/com/company/utils/InitStrategy/InitStrategy.java
@@ -1,10 +1,24 @@
 package com.company.utils.InitStrategy;
 
 /**
- * Created by Home on 13.08.2017.
+ * Class {@code InitStrategy} is a specific type of sales objects initialization service
+ * This service is able to retrieve statistical data from CSV files and initialize sales
+ * entities for their further analysis
+ * @param <T> describes objects of sales analysis, i.e. {@code Customer}, {@code Item}
+ * @author Leonid Shelest
  */
 public interface InitStrategy<T> {
-    public String getFileName();
+    /**
+     * Returns name of CSV file containing information about objects of analysis
+     * Keep in mind the relative file path will be retrieved
+     * @return String value of CSV file name
+     */
+    String getFileName();
 
-    public T parseLine(String[] parts);
+    /**
+     Returns an object of sales analysis initialized with values retrieved from one line of text file
+     @return Object of sales analysis
+
+     */
+    T parseLine(String[] parts);
 }
